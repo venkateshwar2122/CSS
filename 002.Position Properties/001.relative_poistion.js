29fb7e5e3f22915149230ff3ad9ddce543e69c81
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-11-08 13:21:39 +0530

    Create 001.relative_poistion.js

---
+++ 002.Position Properties/001.relative_poistion.js
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div{
+            width: 200px;
+            height: 100px;
+        }
+        .con1{
+            background-color: blue;
+            position: relative;
+            bottom: -20px;
+        }
+        .con2{
+            background-color: yellow;
+        }
+        .con3{
+            background-color: green;
+            position: relative;
+            right:30px;
+            bottom: 50px;
+        }
+        .con4{
+            background-color: red;
+            position: relative;
+            left:100px;
+            top: 30px;
+        }
+    </style>
+</head>
+<body>
+    <div class="con1"></divc>
+    <div class="con2"></divc>
+    <div class="con3"></divc>
+    <div class="con4"></divc>
+</body>
+
+</html>

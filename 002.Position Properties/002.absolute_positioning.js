abfde60d9d4691c814b82405fe573d269b595099
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-11-08 13:22:31 +0530

    Create 002.absolute_positioning.js

---
+++ 002.Position Properties/002.absolute_positioning.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div{
+            width: 200px;
+            height: 100px;
+            position: absolute
+        }
+        .con1{
+            background-color: red;
+        }
+        .con2{
+            background-color: blue;
+            top: 20px;
+            left: 20px;
+        }
+        .con3{
+            background-color: green;
+            top: 40px;
+            left: 40px;
+        }
+        .con4{
+            background-color: yellow;
+            top: 60px;
+            left: 60px;
+        }
+    </style>
+</head>
+<body>
+    <div class="con1"></div>
+    <div class="con2"></div>
+    <div class="con3"></div>
+    <div class="con4"></div>    
+</body>
+</html>
